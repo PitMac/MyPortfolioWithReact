dca25126be9da2d099c6d07425d0f4f85123c943
PitMac <jhondavidpitapita@hotmail.com>
2022-03-13 19:57:31 -0500

    Add logo and name of application

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import ThemeButton from "./components/ThemeButton";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
+import "aos/dist/aos.css";
 import AOS from "aos";
 
 function App() {
